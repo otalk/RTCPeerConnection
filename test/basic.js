013b1e5769204d9d581b06d191ece70e1f7933c7
Philipp Hancke <fippo@andyet.net>
2014-03-08 12:32:45 +0100

    use jingle

--- test/basic.js
+++ test/basic.js
@@ -46,6 +46,50 @@ test('basic connection establishment', function (t) {
     pc1.offer();
 });
 
+test('basic connection establishment -- using Jingle', function (t) {
+    var pc1, pc2;
+    pc1 = new PeerConnection({useJingle: true});
+    pc2 = new PeerConnection({useJingle: true});
+
+    pc1.on('ice', function (candidate) {
+        pc2.processIce(candidate);
+    });
+    pc2.on('ice', function (candidate) {
+        pc1.processIce(candidate);
+    });
+
+    pc1.on('iceConnectionStateChange', function () {
+        //console.log('pc1 iceConnectionStateChange', pc1.pc.iceConnectionState);
+        if (pc1.pc.iceConnectionState == 'connected') {
+            t.end();
+        }
+        // FIXME: also look for https://code.google.com/p/webrtc/issues/detail?id=1414
+    });
+    pc2.on('iceConnectionStateChange', function () {
+        //console.log('pc2 iceConnectionStateChange', pc2.pc.iceConnectionState);
+    });
+
+    pc1.on('offer', function (offer) {
+        pc2.handleOffer(offer, function (err) {
+            if (err) {
+                // handle error
+                t.fail('error handling offer');
+                return;
+            }
+
+            pc2.answer(function (err, answer) {
+                if (err) {
+                    // handle error
+                    t.fail('error handling answer');
+                    return;
+                }
+                pc1.handleAnswer(answer);
+            });
+        });
+    });
+    pc1.offer();
+});
+
 test('async accept', function (t) {
     var pc1, pc2;
     pc1 = new PeerConnection();
