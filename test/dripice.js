52b5295a33ae613849027a63a28ae541fa040505
Philipp Hancke <fippo@andyet.net>
2015-07-15 10:45:36 -0700

    make this test reliable, too

--- test/dripice.js
+++ test/dripice.js
@@ -24,9 +24,13 @@ test('batching trickle ice candidates', function (t) {
 
     pc1.on('iceConnectionStateChange', function () {
         //console.log('pc1 iceConnectionStateChange', pc1.iceConnectionState);
-        if (pc1.iceConnectionState == 'connected') {
-            t.pass('P2P connection established');
-            t.end();
+        if (pc1.iceConnectionState === 'connected' ||
+          pc1.iceConnectionState === 'completed') {
+            if (!ended) {
+                t.pass('P2P connection established');
+                ended = true;
+                t.end();
+            }
         }
         // FIXME: also look for https://code.google.com/p/webrtc/issues/detail?id=1414
     });
