8a9b409a39b804d53f541819539ee0b61bdf2117
Xander Dumaine <xander.dumaine@inin.com>
2016-07-13 15:23:32 -0400

    Make bundle loadable in non-es6 browsers

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
-var bundle = require('browserify')({standalone: 'PeerConnection'}),
-    fs = require('fs');
+var bundle = require('browserify')({
+    standalone: 'PeerConnection'
+});
+var fs = require('fs');
 
 
 bundle.add('./rtcpeerconnection');
+bundle.transform("babelify", {
+  global: true,
+  ignore: /\/node_modules\/(?!webrtc-adapter\/)/
+});
 bundle.bundle().pipe(fs.createWriteStream('rtcpeerconnection.bundle.js'));
